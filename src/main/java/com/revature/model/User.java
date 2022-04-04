8aa1e31dcb30f2211b549be684d391d153b4055e
Ankur Shahi <shahiankur80@gmail.com>
2022-04-04 14:25:09 -0400

    Added Reimbursement model

--- src/main/java/com/revature/model/User.java
+++ src/main/java/com/revature/model/User.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)// autoincrements
+    @GeneratedValue(strategy = GenerationType.IDENTITY)// auto increments
     @Column(name="user_id")
     private int id;
 
