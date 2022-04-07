c6afcca232b07c8b5902b3a1ca50a19593209626
Ankur Shahi <shahiankur80@gmail.com>
2022-04-07 10:55:55 -0400

    User Entity added

---
+++ revature-demo/src/main/java/com/revature/model/User.java
@@ -0,0 +1,108 @@
+package com.revature.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity // Tells Hibernate that this is a table
+@Table(name="ers_users") // Optional, but allows us to define out table like we want
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto increments
+    @Column(name="user_id")
+    private int id;
+
+    @Column(name="user_firstname", nullable = false)
+    private String firstName;
+
+    @Column(name="user_lastname", nullable = false)
+    private String lastName;
+
+    @Column(name="username", nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(name="password", nullable = false, length = 50)
+    private String password;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String username, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+    }
+
+    public User(int id, String firstName, String lastName, String username, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName) && Objects.equals(username, user.username) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, username, password);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
